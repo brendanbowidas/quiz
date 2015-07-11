f2357205ea0a3ffb3024d342edb3e93e9c58da0e
brendanbowidas <brendanbowidas@gmail.com>
2015-07-11 17:43:38 -0400

    "began adding functionality: created question objects and functions to append them"

---
+++ js/app.js
@@ -0,0 +1,54 @@
+//set global variables
+
+var points = 0;
+var qNum = 0;
+
+
+
+
+
+//set questions
+function Question(pic, choices, answer) {
+    this.pic = pic;
+    this.choices = choices;
+    this.answer = answer;
+
+}
+
+var q1 = new Question("img/bicep.png",["Vastus Medialis", "Latissimus Dorsi", "Trapezius", "Biceps Brachii"], "Biceps Brachii" );
+
+var q2 = new Question("img/pec.png", ["Rectus Femoris", "Teres Minor", "Pectoralis Minor", "Pectoralis Major"], "Pectoralis Major");
+
+var q3 = new Question("img/quad.png", ["Lattisimus Dorsi", "Gastrocnemius", "Deltiod", "Quadriceps Femoris"], "Quadriceps Femoris");
+
+var q4 = new Question("img/lat.png", ["Soleus", "Rectus Abdominus", "Serratus Anterior", "Lattisimus Dorsi"], "Lattisimus Dorsi");
+
+var q5 = new Question("img/gastro.png", ["Vastus Medialis", "Adductor Brevis", "Brachioradialis", "Gastrocnemius"], "Gastrocnemius");
+
+var questions = [q1, q2, q3, q4, q5];
+
+//checks answer
+function checkAnswer() {
+
+
+
+};
+
+//shows question
+function showQuestion() {
+    var current = questions[qNum];
+    var currChoices = current.choices;
+    $(".image img").attr("src", current.pic);
+    $(".count").text(qNum +1);
+
+    for(var i = 0; i < current.choices.length; i++){
+        $(".answers").prepend("<li>" + currChoices[i] + "</li>")
+    }
+};
+
+$(document).ready(function(){
+    showQuestion();
+});
+
+
+
